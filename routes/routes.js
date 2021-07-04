3448c1862c0bdf3ac184fd8ac5999d70416277ab
devanshu <devanshukumar45@gmail.com>
2021-07-04 14:50:51 +0530

    added multiple functionalities

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,87 @@
 const router = require('express').Router();
-const Hero = require('../models/hero')
+const User = require('../models/user');
+const Asset = require('../models/asset');
 
-router.get('/', async (req,res)=>{
-    const data =  await Hero.find();
-    res.json(data);
-    // res.send(`routed get ${req.params['id']}`);
+
+// USER RELATED REQUESTS
+
+router.get('/get-users', async (req,res)=>{
+    const userData =  await User.find();
+    res.json(userData);
+    console.log(userData);
+});
+
+//LOGIN
+router.post('/login', async (req, res)=>{
+    // const userData = await User.find();
+    var username = req.body.username;
+    var password = req.body.password;
+
+    
+    if(username.length != 0 && password.length != 0){
+        // BOTH USERNAME AND PASSWORD ARE PRESENT
+        res.status(202).json({message: 'valid'});
+    }else{
+        if(username.length == 0 && password.length == 0){
+            // BOTH USERNAME AND PASSWORD ARE ABSENT
+            res.status(410).json({message: 'invalid credentials'});
+        }
+        else{
+            if(username.length == 0){
+                // USERNAME IS ABSENT
+                res.status(409).json({message: 'invalid username'});
+            }else{
+                // PASSWORD IS ABSENT
+                res.status(408).json({message: 'invalid password'});
+            }
+        }
+    }
+
+});
+
+// REGISTRATION
+router.post('/reg/user', async (req, res)=>{
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });    
+    try{
+        const newUser = await user.save(); 
+        res.status(201).json({message: 'new user created', user: newUser});
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
 });
 
 
+// ASSET RELATED REQUESTS
+
+router.get('/get-assets', async (req, res)=>{
+    const assetData = await Asset.find();
+    res.json(assetData);
+    console.log(assetData);
+
+
+});
+
+router.post('/reg/asset', async (req,res)=>{
+    const asset = new Asset({
+        name: req.body.name,
+        price: req.body.price,
+        minimumBid: req.body.minimumBid,
+        openingDate: req.body.openingDate,
+        closingDate: req.body.closingDate
+    });
+
+    try{
+        const newAsset = await asset.save();
+        res.status(201).json({message: 'new asset added', asset: newAsset});
+    }catch(error){
+        res.status(400).json({message: 'bad request'});
+    }
+});
+
 module.exports = router;
+
+
+// CREATE MORE ROUTES
