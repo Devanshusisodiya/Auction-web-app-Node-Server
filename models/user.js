af305eb63c0399b9a9a483f3e9cd1a028f25a200
devanshu <devanshukumar45@gmail.com>
2021-07-04 10:20:51 +0530

    created models

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const User = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('user', User);
