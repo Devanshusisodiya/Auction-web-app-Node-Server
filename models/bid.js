33cf2ab2f3f9535c505dc31fc6b75710c17c9c9f
devanshu <devanshukumar45@gmail.com>
2021-07-08 05:48:46 +0530

    created bidding model

---
+++ models/bid.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const bidSchema = mongoose.Schema({
+    assetName: {
+        type: String,
+        required: true
+    },
+    bidders: {
+        type: Array,
+        required: false
+    },
+});
+
+module.exports = mongoose.model('bid', bidSchema);
