a5c72335e262a26ddfc7c4740737f5baea93ed29
devanshu <devanshukumar45@gmail.com>
2021-07-12 04:50:49 +0530

    reconfiged sever

--- server.js
+++ server.js
@@ -17,12 +17,6 @@ var options = {
   }
   app.use(cors(options));
 
-const PORT = process.env.PORT;
-const routes = require('./routes/routes');
-
-app.use(express.json());
-app.use('/api', routes);
-
 
 const mongoose = require('mongoose');
 // REQUEST CONNECTION TO DATABASE
@@ -38,6 +32,11 @@ const db = mongoose.connection;
 db.on('error', (error)=>{console.log(error)});
 db.once('open', ()=>{console.log('Connected to Database')});
 
+const PORT = process.env.PORT;
+const routes = require('./routes/routes');
+
+app.use(express.json());
+app.use('/api', routes);
 
 // API BASE POINT
 app.get('/', (req,res)=>{
